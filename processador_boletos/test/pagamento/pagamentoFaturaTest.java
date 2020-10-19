d197a56a0e7d2e33347f2fb5709e9bb1a877b82b
pramilson <ramilson_silva@id.uff.br>
2020-10-18 22:52:39 -0300

    Criado classe de teste (próximo passo - criar classes para executar o teste

---
+++ processador_boletos/test/pagamento/pagamentoFaturaTest.java
@@ -0,0 +1,13 @@
+package pagamento;
+
+import org.junit.jupiter.api.Test;
+
+public class pagamentoFaturaTest {
+	
+	@Test
+	public void pagamentoFatura() {
+		pagamento1.pagamentoFaturaPorBoleto();
+		assertEquals(true,fatura1.paga)
+	}
+
+}
