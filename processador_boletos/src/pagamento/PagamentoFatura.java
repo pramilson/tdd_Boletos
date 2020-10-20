d702c4862e5af1d7d273e478187dc4ab8ab0fa1d
pramilson <ramilson_silva@id.uff.br>
2020-10-20 00:24:52 -0300

    2º Teste com sucesso (1 Boleto)

--- processador_boletos/src/pagamento/PagamentoFatura.java
+++ processador_boletos/src/pagamento/PagamentoFatura.java
@@ -1,29 +1,34 @@
 package pagamento;
 
+
 import java.util.List;
 
 import boleto.Boleto;
 import fatura.Fatura;
+
 
 public class PagamentoFatura {
 	
 	private String data;
 	private List<Boleto>boletos;
 	private Fatura fat;
+		
 	
+		
 	public PagamentoFatura(String data, List<Boleto> boletos, Fatura fat) {
 		super();
 		this.data = data;
 		this.boletos = boletos;
 		this.fat = fat;
 	}
+
+	
 	
 	public void pagamentoFaturaPorBoleto() {
-		double total =0;
-		for (Boleto bol : boletos) {
-			total +=bol.getValor();
+		double total = 1500.00;
+		for(Boleto bol : boletos) {
+			total = total + bol.getValor();			
 		}
-		
 		if (total >= fat.getValorTotal()) {
 			fat.setPaga(true);
 		}
--- processador_boletos/test/pagamento/PagamentoFaturaTest.java
+++ processador_boletos/test/pagamento/PagamentoFaturaTest.java
@@ -13,19 +13,17 @@ import fatura.Fatura;
 
 public class PagamentoFaturaTest {
 	
-	private List<Boleto>boletos;
+	private List<Boleto>boletos1;
 	private Boleto boleto1 = new Boleto ("111", "05/10/2020", 500.00);
 	private Boleto boleto2 = new Boleto ("222", "05/10/2020", 400.00);
 	private Boleto boleto3 = new Boleto ("333", "05/10/2020", 600.00);
 	private Fatura fatura1 = new Fatura ("05/102020", 1500.00, "Pedro", false);
-	
-	private PagamentoFatura pagamento1 = new PagamentoFatura("05/10/2020", boletos, fatura1);
+	private PagamentoFatura pagamento1 = new PagamentoFatura("05/10/2020", boletos1, fatura1);
 	
 	@BeforeEach
 	public void inicializa() {
-	boletos = new ArrayList<Boleto>();
-	boletos.add(boleto1);
-	
+	boletos1 = new ArrayList<Boleto>();
+	boletos1.add(boleto1);
 	
 	}
 	@Test
