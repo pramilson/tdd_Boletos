ecea9a798bfdef6cea1c6027215e6ddd41131fac
pramilson <ramilson_silva@id.uff.br>
2020-10-20 00:27:23 -0300

    3º Teste com falta (2 Boletos)

--- processador_boletos/test/pagamento/PagamentoFaturaTest.java
+++ processador_boletos/test/pagamento/PagamentoFaturaTest.java
@@ -13,18 +13,18 @@ import fatura.Fatura;
 
 public class PagamentoFaturaTest {
 	
-	private List<Boleto>boletos1;
+	private List<Boleto>boletos;
 	private Boleto boleto1 = new Boleto ("111", "05/10/2020", 500.00);
 	private Boleto boleto2 = new Boleto ("222", "05/10/2020", 400.00);
 	private Boleto boleto3 = new Boleto ("333", "05/10/2020", 600.00);
 	private Fatura fatura1 = new Fatura ("05/102020", 1500.00, "Pedro", false);
-	private PagamentoFatura pagamento1 = new PagamentoFatura("05/10/2020", boletos1, fatura1);
+	private PagamentoFatura pagamento1 = new PagamentoFatura("05/10/2020", boletos, fatura1);
 	
 	@BeforeEach
 	public void inicializa() {
-	boletos1 = new ArrayList<Boleto>();
-	boletos1.add(boleto1);
-	
+	boletos = new ArrayList<Boleto>();
+	boletos.add(boleto1);
+	boletos.add(boleto2);
 	}
 	@Test
 	public void pagamentoFatura() {
